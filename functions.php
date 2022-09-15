0979ce193d529c6307168e7cab8dae039895e083
Hiraya8823 <hirayama82007@yahoo.co.jp>
2022-09-15 09:58:06 +0900

    登録処理の完成

--- functions.php
+++ functions.php
@@ -3,8 +3,6 @@
 // 設定ファイルを読み込む
 require_once __DIR__ . '/config.php';
 
-
-
 // 接続処理を行う関数
 function connect_db()
 {
@@ -29,13 +27,52 @@ function h($str)
 {
     // ENT_QUOTES: シングルクオートとダブルクオートを共に変換する。
     return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
-} 
+}
+
+// タスク登録時のバリデーション
+function insert_validate($title)
+{
+    // 初期化
+    $errors = [];
+
+    if (empty($title)) {
+        $errors[] = MSG_TITLE_REQUIRED;
+    }
+
+    return $errors;
+}
+
+// タスク登録
+function insert_task($title)
+{
+    // データベースに接続
+    $dbh = connect_db();
+
+    // レコードを追加
+    $sql = <<<EOM
+    INSERT INTO
+        tasks
+        (title)
+    VALUES
+        (:title)
+    EOM;
+
+    // プリペアドステートメントの準備
+    $stmt = $dbh->prepare($sql);
+
+    // パラメータのバインド
+    $stmt->bindValue(':title', $title, PDO::PARAM_STR);
+
+    // プリペアドステートメントの実行
+    $stmt->execute();
+}
 
 function find_task_by_done($status)
 {
     $dbh = connect_db();
+
     /* タスク照会
-    ---------------------------------------------*/
+---------------------------------------------*/
     // done を抽出条件に指定してデータを取得
     $sql = <<<EOM
     SELECT
@@ -57,4 +94,4 @@ function find_task_by_done($status)
 
     // 結果の取得
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
-}
+} 
